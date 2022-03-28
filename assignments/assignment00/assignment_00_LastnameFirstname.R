bccbc9b6e31feb2620512a8de39fb68077a057e6
JoshuaShepard87 <Joshua.burden@beeso.io>
2022-03-27 21:42:49 -0500

    commiting code

--- assignments/assignment00/assignment_00_LastnameFirstname.R
+++ assignments/assignment00/assignment_00_LastnameFirstname.R
@@ -1,52 +1,67 @@
 # Assignment: ASSIGNMENT 0
-# Name: Lastname, Firstname
-# Date: 2010-02-14
+# Name: Burden, Joshua
+# Date: 2022-03-27
 
 # Basics
 
 ## Add 8 and 5
-___
-
+x <- 8
+y <- 5
+z <- x+y
+print(z)
 ## Subtract 6 from 22
-___
+a <- 6
+b <- 22
+c <- b-a
+print(c)
 
 ## Multiply 6 by 7
-___
+d <- 6
+e <- 7
+f <- d*e
+print(f)
 
 ## Add 4 to 6 and divide the result by 2
-___
+g <- 4
+h <- 6
+i <- (g + h / 2)
+print(i)
 
 ## Compute 5 modulo 2
-___
+aa <- (5 %% 2)
+print(aa)
 
 ## Assign the value 82 to the variable x
 ## Print x
-x <- ___
-x
+x <- 82
+print(x)
 
 ## Assign the value 41 to the variable y
 ## Print y
-y <- ___
-___
+y <- 41
+print(y)
 
 ## Assign the output of x + y to the variable z
 ## Print z
-z <- ___
-___
+z <- x + y
+print(z)
 
 ## Assign the string value "DSC520" to the variable class_name
 ## Print the value of class_name
-class_name <- ___
+class_name <- "DSC520"
+print(class_name)
 
 ## Assign the string value of TRUE to the variable is_good
 ## Print the value of is_good
-is_good <- ___
+is_good <- TRUE
+print(is_good)
 
 ## Check the class of the variable is_good using the `class()` function
-___
+class(is_good)
 
 ## Check the class of the variable z using the `class()` function
-___
+class(z)
 
 ## Check the class of the variable class_name using the class() function
-___
+class(class_name)
+
